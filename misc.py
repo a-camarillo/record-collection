678f9bbbc1ed79089d4f10e4305a3bf93c8159f7
a-camarillo <anthony.camarillo.96@gmail.com>
2020-12-15 07:56:27 -0800

    added extract_consecutive_elements to misc.py

--- misc.py
+++ misc.py
@@ -56,9 +56,26 @@ def milliseconds_to_minutes(milliseconds:int):
 
     return time_string
 
+def extract_consecutive_elements(element_list:list,num_elements:int):
+    '''
+    This function will extract a consecutive number of elements from a list and return the extracted list
 
+        Attributes: 
+            element_list -- a list from which elements should be extracted from
+            num_elements -- an integer representing number of consecutive elements to extract
+        Returns:
+            segmented_list -- the list broken down into segmented list of lists
+    '''
+    segmented_list = []
+    
+    for i in range(0,len(element_list),num_elements):
+        
+        segmented_list.append(element_list[i:i+num_elements])
+        
+        if (len(element_list) - i) < num_elements: #trivial check for elements between last step and end of list
+            segmented_list.append(element_list[i:len(element_list)])
 
- 
+    return segmented_list
 
 
 
